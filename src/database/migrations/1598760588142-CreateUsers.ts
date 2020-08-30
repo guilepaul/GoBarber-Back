677dd397d44eaee0812fe06f99e405a67ad181cd
Guilherme Di Paula <guilepaul@gmail.com>
2020-08-30 01:55:13 -0300

    users modal and migration

--- src/database/migrations/1598760588142-CreateUsers.ts
+++ src/database/migrations/1598760588142-CreateUsers.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1598754205194
-  implements MigrationInterface {
+export default class CreateUsers1598760588142 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,12 +14,17 @@ export default class CreateAppointments1598754205194
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
