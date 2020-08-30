68039cc51ac2da6df56c9898cf4f0981086a9626
Guilherme Di Paula <guilepaul@gmail.com>
2020-08-30 17:06:09 -0300

    models relationship

--- src/database/migrations/1598760588142-CreateUsers.ts
+++ src/database/migrations/1598760588142-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1598760588142 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
