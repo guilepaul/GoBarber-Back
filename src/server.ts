50349bca3724e45a636b07ac56c82f77edc601d8
Guilherme Di Paula <guilepaul@gmail.com>
2020-08-31 23:22:14 -0300

    static files

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,15 @@
 import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
-app.get('/', (request, response) =>
-  response.json({ message: 'Hello Guile Paul' }),
-);
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333');
